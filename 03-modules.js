8be281b9f0e31f757b165324d9b0e6cd8c0814f2
viewtifult <tashanduncan1994@gmail.com>
2022-06-04 10:16:16 +0100

    references in 03 modules

--- 03-modules.js
+++ 03-modules.js
@@ -1,6 +1,9 @@
 // CommonJS, every file is module (by default)
 // Modules - Encapsulated Code (only share minimum)
-const names = require('./04-names')
-const sayHi = require('./05-utils')
-const data = require('./06-alternative-flavor')
-require('./07-mind-grenade')
+const names = require('./04-names');
+const sayHi = require('./05-utils');
+const data = require('./06-alternative-flavor');
+require('./07-mind-grenade');
+sayHi(names.john);
+sayHi('Tashan');
+sayHi(names.peter);
